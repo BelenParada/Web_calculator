7049fcb76122ec26b500058f3cd8e6d148974889
Personal <Personal@PERSONAL>
2021-01-14 20:59:19 -0400

    Agregar archivos de proyecto.

---
+++ Controller/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCalculadoraa.Properties.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+
+    public class CalculadoraaController : ControllerBase
+    {
+        [HttpGet]
+        [Route("suma")]
+        public int suma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        [HttpPost]
+        [Route("suma")]
+        public int sumar([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
